4b61c6f53c3efe46ece1ba45dc244dbdab9dc361
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-17 22:46:58 -0400

    stubbed out methods, created a few tests and stubbed out the rest

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,101 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    private class Node{
+        private Object thePackage;
+        private Node next;
+
+        public Node(){
+            this.thePackage=null;
+            this.next=this;
+        }
+
+        public Node(Object obj){
+            this.thePackage=obj;
+            this.next=this;
+        }
+
+        public Node(Object obj,Node next){
+            this.thePackage=obj;
+            this.next=next;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Object getThePackage() {
+            return thePackage;
+        }
+
+        public void setThePackage(Object thePackage) {
+            this.thePackage = thePackage;
+        }
+    }
+
+    private Node first;
+
+    public SinglyLinkedList(){
+        this.first=new Node();
+        this.first.next = this.first;
+    }
+
+    public SinglyLinkedList(Object obj){
+        this.first=new Node(obj);
+        this.first.next = this.first;
+    }
+
+    public void add(Object obj){
+
+    }
+
+
+    public void remove(Object obj){
+
+    }
+
+    public Boolean contains(Object obj){
+
+        return false;
+    }
+
+    public Integer find(Object obj){
+
+        return -1;
+    }
+
+    public Integer size(){
+
+        return -1;
+    }
+
+    public Object get(Integer i){
+
+        return null;
+    }
+
+    public SinglyLinkedList copy(){
+
+        return null;
+    }
+
+    public void sort(){
+
+
+    }
+
+    public void reverse(){
+
+    }
+
+    public SinglyLinkedList slice(Integer start, Integer stop){
+
+        return null;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,116 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void sllNullaryConstructorTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertNull(sll.get(0));
+    }
+
+    @Test
+    public void sllConstructorTest(){
+        SinglyLinkedList sll = new SinglyLinkedList(5);
+        Object expeccted = 5;
+        Object actual = sll.get(0);
+
+        Assert.assertEquals(expeccted,actual);
+    }
+
+    @Test
+    public void sllAddTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllRemoveTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllRemoveNotThereTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllContainsTrueTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllContainsFalseTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllFindTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllFindNotThereTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllFindMultipleTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllSizeTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllSize0Test(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllGetTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllGetNotThereTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllCopyTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllCopyEmptyTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void sllSortTest(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Assert.assertTrue(false);
+    }
+
 }
